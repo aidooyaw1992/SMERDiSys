0a08d75c0b4b9be74268b2a141435853a671525d
Daniel Aidoo <daniel@africaloop.com>
2018-05-01 14:10:12 +0000

    sqlite

--- models/index.js
+++ models/index.js
@@ -4,14 +4,15 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
+var env       = process.env.NODE_ENV || 'test';
 var config    = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 var db        = {};
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  // var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize('sql2235084', 'sql2235084', 'wii7fw6b', {host:'sql2.freemysqlhosting.net', dialect:'mysql',operatorsAliases:false});
 }
 
 fs
